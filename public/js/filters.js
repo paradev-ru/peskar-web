63fbe9f02de12e1d5a7dc9f688f462845439df47
leoderoko <leoderoko@gmail.com>
2016-11-08 14:31:46 +0500

    Time diff

--- public/js/filters.js
+++ public/js/filters.js
@@ -9,4 +9,27 @@ angular.module("app")
             }
             return true;
         };
+    })
+    .filter('diff', function() {
+        return function(input) {
+            input = new Date(input)
+            var difference = new Date().getTime() - input;
+            var seconds = Math.floor(difference/1000);
+            var minutes = Math.floor(seconds/60);
+            var hours = Math.floor(minutes/60);
+            var days =  Math.floor(hours/24);
+            var result = null;
+
+            if (days) {
+                result = days === 1 ? '1 day' : days + ' days';
+            } else if (hours) {
+                result = hours === 1 ? '1 hour' : hours + ' hours';
+            } else if (minutes) {
+                result = minutes === 1 ? '1 minute' : minutes + ' minutes';
+            } else {
+                result = seconds === 1 ? '1 second' : seconds + ' seconds';
+            }
+
+            return result;
+        };
     });
