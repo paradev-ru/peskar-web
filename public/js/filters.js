7bd188cbfdd4c752ac2c4491eeb2d5ba761ace7d
leoderoko <leoderoko@gmail.com>
2016-11-09 21:46:32 +0500

    Add nt2br filter

--- public/js/filters.js
+++ public/js/filters.js
@@ -36,6 +36,15 @@ angular.module('myApp.filters', [])
         };
     })
 
+    .filter('nl2br', function($sce){
+        return function(msg,is_xhtml) {
+            var is_xhtml = is_xhtml || true;
+            var breakTag = (is_xhtml) ? '<br />' : '<br>';
+            var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+            return $sce.trustAsHtml(msg);
+        }
+    })
+
     .filter('filename', function() {
         return function(input) {
             filename = input.replace(/^.*[\\\/]/, '')
